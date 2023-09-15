08b806d713a6dfee701eda5800b053a7f9e34fc8
AlejandroRecarteR <alejandro.recarte.rebollo@gmail.com>
2023-09-15 10:14:01 +0200

    > 15/09/2023 (2)

--- src/Repaso/OptionPane.java
+++ src/Repaso/OptionPane.java
@@ -10,7 +10,7 @@ public class OptionPane {
 
     public static void jOptionPane(){
         String letra = JOptionPane.showInputDialog(null, "Introduzca una letra");
-        if (letra == letra.toUpperCase()){
+        if (letra.equals(letra.toUpperCase())){
             JOptionPane.showConfirmDialog(null, "Es mayúscula", "UwU", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
         }else{
             JOptionPane.showConfirmDialog(null, "Es minúscula", "UwU", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
