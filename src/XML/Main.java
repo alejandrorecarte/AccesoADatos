882f232f4054290e9fa280e267c3b2d428cb5be8
alejandrorecarte <alejandro.recarte.rebollo@gmail.com>
2023-12-08 20:13:41 +0100

    > 8/12/2023

---
+++ src/XML/Main.java
@@ -0,0 +1,2 @@
+package XML;public class Ejercicio3 {
+}
---
+++ src/XML/SimpleErrorHandler.java
@@ -0,0 +1,23 @@
+package XML;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+public class SimpleErrorHandler implements ErrorHandler {
+
+    @Override
+    public void warning(SAXParseException exception) throws SAXException {
+        System.out.println("Warning: " + exception.getMessage());
+    }
+
+    @Override
+    public void error(SAXParseException exception) throws SAXException {
+        System.out.println("Error: " + exception.getMessage());
+    }
+
+    @Override
+    public void fatalError(SAXParseException exception) throws SAXException {
+        System.out.println("Fatal Error: " + exception.getMessage());
+        throw exception;
+    }
+}
